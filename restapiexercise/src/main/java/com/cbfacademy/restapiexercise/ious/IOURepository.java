e6458950a33704f5eee4156eb5c57726f4c1af3f
Kelly Nya <knya819@gmail.com>
2024-08-14 13:28:10 +0100

    created IOURepo.java

---
+++ restapiexercise/src/main/java/com/cbfacademy/restapiexercise/ious/IOURepository.java
@@ -0,0 +1,9 @@
+package com.cbfacademy.restapiexercise.ious;
+
+import org.springframework.data.repository.ListCrudRepository;
+import java.util.UUID;
+
+
+public interface IOURepository extends ListCrudRepository<IOU, UUID>{
+
+}
