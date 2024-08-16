534428420fb2a61f6d82ff86fedcefba5d55cde4
Kelly Nya <knya819@gmail.com>
2024-08-16 10:47:41 +0100

    modified IOURepo file

--- restapiexercise/src/main/java/com/cbfacademy/restapiexercise/ious/IOURepository.java
+++ restapiexercise/src/main/java/com/cbfacademy/restapiexercise/ious/IOURepository.java
@@ -1,13 +1,9 @@
 package com.cbfacademy.restapiexercise.ious;
 
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
-
 import java.util.UUID;
 
-@Repository
+import org.springframework.data.repository.ListCrudRepository;
+
 public interface IOURepository extends ListCrudRepository<IOU, UUID>{
-    List<IOU> findByBorrower(String borrower);
 
 }
